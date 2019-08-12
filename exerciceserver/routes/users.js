900bd2a29a5833cd86ffd03454615b1607c8a456
Unknown <mohamedridha.mejri@esprit.tn>
2019-08-12 17:50:18 +0100

    user adding

--- exerciceserver/routes/users.js
+++ exerciceserver/routes/users.js
@@ -38,6 +38,25 @@ router.post('/add', function(req, res, next){
     user.phoneNumber= req.sanitize('phoneNumber').escape().trim();
     console.log(user)
 	
+request.post('https://dev.api.clacdesdoigts.com/v2/user',
+{
+  json: {
+    email: user.email,
+    password:user.password,
+    firstName:user.firstName,
+    lastName:user.lastName,
+    phoneNumber:user.phoneNumber
+  }
+},
+(error, rez, body) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  //console.log(`statusCode: ${res.statusCode}`)
+  console.log(body)
+  console.log(body.result);
+  if(body.result=="error"){
     res.render('user/add', { 
       title: 'Add New User',
 
@@ -46,10 +65,14 @@ router.post('/add', function(req, res, next){
         firstName:  req.body.firstName,
         lastName:  req.body.lastName		,
         phoneNumber:  req.body.phoneNumber		,
-        merr:null,
-        msucc:"success"
+        merr:body.data[0].message,
+        msucc:null
       
   })
+  }
+}
+)
+
 	}
 	else {   //Display errors to user
 		var error_msg = ''
@@ -72,5 +95,34 @@ router.post('/add', function(req, res, next){
         })
     }
 })
+
+/* get token from CDD API. */
+router.post('/login', function(req, rez) {
+  let data = JSON.stringify({
+    password: "Asdfasdf00",
+    username:  "apitest001@clacdesdoigts.com"
+})
+
+
+request.post('https://dev.api.clacdesdoigts.com/v2/auth/token',
+{
+  json: {
+    password: "Asdfasdf00",
+    username:  "apitest001@clacdesdoigts.com"
+  }
+},
+(error, res, body) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  //console.log(`statusCode: ${res.statusCode}`)
+  console.log(body)
+  rez.send(body)
+}
+)
+
+})
+
 
 module.exports = router;
